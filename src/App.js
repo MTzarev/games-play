c0808f0a638564f09dc5d07b9c3144558ceb3323
Miroslav Tzarev <mtzarev@gmail.com>
2024-12-16 18:34:21 +0200

    Add routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import Home from "./components/Home";
 import WelcomeWorld from "./components/WelcomeWorld";
+import CatalogPage from "./components/CatalogPage";
+import CreatePage from "./components/CreatePage";
+import LoginPage from "./componenets/LoginPage";
+import RegisterPage from "./components/RegisterPage"
+
 function App() {
+  let routes ={
+    '/home': WelcomeWorld,
+    '/games': CatalogPage,
+    '/create-game': CreatePage,
+    '/login': LoginPage, 
+    '/register': RegisterPage
+  }
   return (
     
     <div id="box">
-          <Home></Home>
+          <Home/>
 
     <main id="main-content">
       <WelcomeWorld/>
